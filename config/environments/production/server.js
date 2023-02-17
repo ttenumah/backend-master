5ec47626eae1482d6422dfbeb42a8dc62b5e4427
ttenumah <ttenumah@gmail.com>
2023-02-16 21:55:03 -0800

    first commit

---
+++ config/environments/production/server.js
@@ -0,0 +1,10 @@
+// Path: ./config/env/production/server.js
+
+module.exports = ({ env }) => ({
+    proxy: true,
+    url: env('APP_URL'), // Sets the public URL of the application.
+    app: { 
+      keys: env.array('APP_KEYS')
+    },
+});
+
